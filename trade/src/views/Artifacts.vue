d5b3148d3cae8cc4fe4ed3fcd90b3521636f2505
Chris Cox <chris.cox@westfall.io>
2024-11-04 16:17:18 -0500

    :bug: Fixed an issue with missing quotes

--- trade/src/views/Artifacts.vue
+++ trade/src/views/Artifacts.vue
@@ -9,7 +9,7 @@ const page = ref(1);
 const pages = ref(1);
 
 const api_addr = ref("https://windstorm-api.digitalforge.app")
-const artifact_addr = ref(https://configs.digitalforge.app)
+const artifact_addr = ref("https://configs.digitalforge.app")
 
 async function getArtifacts(page) {
   fetch(api_addr+'/views/artifacts/?size=10&page='+page.value)
